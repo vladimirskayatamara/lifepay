995186d917789258b26f44682f7d4fe06f521ea2
Toma Vladimirskaya <vladimirskayatamara@gmail.com>
2022-09-14 12:40:51 +0300

    Production build

--- src/js/common.js
+++ src/js/common.js
@@ -18,14 +18,37 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><svg width="39" height="39" viewBox="0 0 39 39" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="19.5" cy="19.5" r="19.5" fill="#3F8DCB" fill-opacity="0.56"/><path d="M26 19.5L26.7071 18.7929L27.4142 19.5L26.7071 20.2071L26 19.5ZM15.2071 7.29289L26.7071 18.7929L25.2929 20.2071L13.7929 8.70711L15.2071 7.29289ZM26.7071 20.2071L15.2071 31.7071L13.7929 30.2929L25.2929 18.7929L26.7071 20.2071Z" fill="white"/></svg></button>'
     })
 
-    // Modal
-/*
-    $('[data-modal=order]').on('click', function() {
-        $('.overlay, #order').fadeIn('slow');
+    var scrollTop = '';
+    var newHeight = '100';
+
+    $(window).bind('scroll', function() {
+       scrollTop = $( window ).scrollTop();
+       newHeight = scrollTop + 100;
+    });
+    
+    $('._btn').click(function(e) {
+       e.stopPropagation();
+     if(jQuery(window).width() < 767) {
+       $(this).after( $( ".popup" ) );
+       $('.popup').show().addClass('popup-mobile').css('top', 0);
+       $('html, body').animate({
+            scrollTop: $('.popup').offset().top
+        }, 500);   
+     } else {
+       $('.popup').removeClass('popup-mobile').css('top', newHeight).toggle();
+     };
+    });
+    
+    $('html').click(function() {
+     $('.popup').hide();
     });
 
-    $('.modal__close').on('click', function() {
-        $('.overlay, #order').fadeOut('slow');
-    })*/
+    $('.popup-btn-close').click(function(e){
+      $('.popup').hide();
+    });
+
+    $('.popup').click(function(e){
+      e.stopPropagation();
+    });
     
 });
